c9871b20567491d19d97461eef5b35280715b5f9
tusharsankhala <tusharsankhala@gmail.com>
2022-07-31 19:29:54 +0530

    Added simple traingle rendering.

--- Src/01_Basic_Engine_SetUp/main.cpp
+++ Src/01_Basic_Engine_SetUp/main.cpp
@@ -1,24 +1,123 @@
-#include "../Common/Scene.h"
-#include "../Common/SceneBasic.h"
-#include "../Common/SceneRunner.h"
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
-#include <memory>
+#include <stdio.h>
+#include <stdlib.h>
 
-const int winWidth = 800;
-const int winHeight = 800;
+static const char* shaderCodeVertex = R"(
 
-static std::map<std::string, std::string> sceneData = {
-	{"basic", "Basic Scene."},
-};
+#version 460 core
+layout (location=0) out vec3 color;
+
+const vec2 pos[3] = vec2[3]
+(
+	vec2(-0.6, -0.4),
+	vec2(0.6, -0.4),
+	vec2(0.0, 0.6)
+);
+
+const vec3 col[3] = vec3[3]
+(
+	vec3(1.0, 0.0, 0.0),
+	vec3(0.0, 1.0, 0.0),
+	vec3(0.0, 0.0, 1.0)
+);
+
+void main()
+{
+	gl_Position = vec4(pos[gl_VertexID], 0.0, 1.0);
+	color = col[gl_VertexID];
+}
+)";
+
+static const char* shaderCodeFragment = R"(
+#version 460 core
+layout (location=0) in vec3 color;
+layout (location=0) out vec4 out_FragColor;
+void main()
+{
+	out_FragColor = vec4(color, 1.0);
+}
+)";
+
 
 int main()
 {
-	SceneRunner runner{ "Chapter1", winWidth, winHeight };
-	runner.Init();
+	glfwSetErrorCallback(
+		[](int error, const char* description)
+		{
+			fprintf(stderr, "Error: %s\n", description);
+		}
+	);
 
-	std::unique_ptr<Scene> scene;
-	scene = std::unique_ptr<Scene>(new SceneBasic(runner.GetWindow(), winWidth, winHeight));
+	if (!glfwInit())
+		exit(EXIT_FAILURE);
 
-	return runner.Run(*scene);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(1024, 768, "OpenGL Window", nullptr, nullptr);
+	if (!window)
+	{
+		glfwTerminate();
+		exit(EXIT_FAILURE);
+	}
+
+	glfwSetKeyCallback(window,
+		[](GLFWwindow* window, int key, int scancode, int action, int mods)
+		{
+			if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+			{
+				glfwSetWindowShouldClose(window, true);
+			}
+		}
+	);
+
+	glfwMakeContextCurrent(window);
+	gladLoadGL();
+	glfwSwapInterval(1);
+
+	const GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &shaderCodeVertex, nullptr);
+	glCompileShader(vertexShader);
+
+	const GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &shaderCodeFragment, nullptr);
+	glCompileShader(fragmentShader);
+
+	const GLuint program = glCreateProgram();
+	glAttachShader(program, vertexShader);
+	glAttachShader(program, fragmentShader);
+
+	glLinkProgram(program);
+	glUseProgram(program);
+
+	GLuint VAO;
+	glCreateVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		int width, height;
+		glfwGetFramebufferSize(window, &width, &height);
+		glViewport(0, 0, width, height);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glDeleteProgram(program);
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+	glDeleteVertexArrays(1, &VAO);
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
+	return 0;
 }
 

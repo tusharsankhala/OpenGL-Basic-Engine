eb8dcca19518db6be25ff65dfe31b849d4e02c90
tusharsankhala <tusharsankhala@gmail.com>
2022-08-07 13:35:16 +0530

    simple rotating cube with interpolated color example.

--- Src/01_Basic_Engine_SetUp/main.cpp
+++ Src/01_Basic_Engine_SetUp/main.cpp
@@ -1,32 +1,73 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb_image_write.h>
 
 #include <stdio.h>
 #include <stdlib.h>
 
+using glm::mat4;
+using glm::vec3;
+
 static const char* shaderCodeVertex = R"(
 
 #version 460 core
+layout(std140, binding = 0) uniform PerFrameData
+{
+	uniform mat4 MVP;
+	uniform int isWireframe;
+};
 layout (location=0) out vec3 color;
-
-const vec2 pos[3] = vec2[3]
-(
-	vec2(-0.6, -0.4),
-	vec2(0.6, -0.4),
-	vec2(0.0, 0.6)
+const vec3 pos[8] = vec3[8](
+	vec3(-1.0,-1.0, 1.0),
+	vec3( 1.0,-1.0, 1.0),
+	vec3( 1.0, 1.0, 1.0),
+	vec3(-1.0, 1.0, 1.0),
+	vec3(-1.0,-1.0,-1.0),
+	vec3( 1.0,-1.0,-1.0),
+	vec3( 1.0, 1.0,-1.0),
+	vec3(-1.0, 1.0,-1.0)
 );
 
-const vec3 col[3] = vec3[3]
+const vec3 col[8] = vec3[8]
 (
 	vec3(1.0, 0.0, 0.0),
 	vec3(0.0, 1.0, 0.0),
-	vec3(0.0, 0.0, 1.0)
+	vec3(0.0, 0.0, 1.0),
+	vec3(1.0, 1.0, 0.0),
+
+	vec3(1.0, 0.0, 0.0),
+	vec3(0.0, 1.0, 0.0),
+	vec3(0.0, 0.0, 1.0),
+	vec3(1.0, 0.0, 0.0)
+);
+
+const int indices[36] = int[36]
+(
+	// front
+	0, 1, 2, 2, 3, 0,
+	// right
+	1, 5, 6, 6, 2, 1,
+	// back
+	7, 6, 5, 5, 4, 7,
+	// left
+	4, 0, 3, 3, 7, 4,
+	// bottom
+	4, 5, 1, 1, 0, 4,
+	// top
+	3, 2, 6, 6, 7, 3
 );
 
 void main()
 {
-	gl_Position = vec4(pos[gl_VertexID], 0.0, 1.0);
-	color = col[gl_VertexID];
+	int idx = indices[gl_VertexID];
+	gl_Position = MVP * vec4(pos[idx], 1.0);
+	color = isWireframe > 0 ? vec3(0.0) : col[idx];
 }
 )";
 
@@ -39,6 +80,12 @@ void main()
 	out_FragColor = vec4(color, 1.0);
 }
 )";
+
+struct PerFrameData
+{
+	mat4 mpv;
+	int isWireframe;
+};
 
 
 int main()
@@ -97,19 +144,63 @@ int main()
 	glCreateVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
 
+	const GLsizeiptr kBufferSize = sizeof(PerFrameData);
+
+	GLuint perFrameDataBuffer;
+	glCreateBuffers(1, &perFrameDataBuffer);
+	glNamedBufferStorage(perFrameDataBuffer, kBufferSize, nullptr, GL_DYNAMIC_STORAGE_BIT);
+	glBindBufferRange(GL_UNIFORM_BUFFER, 0, perFrameDataBuffer, 0, kBufferSize);
+
 	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+	int w, h, comp;
+	const uint8_t* img = stbi_load("../Resources/Textures/ch2_sample3_STB.jpg", &w, &h, &comp, 3);
+
+	GLuint texture;
+	glCreateTextures(GL_TEXTURE_2D, 1, &texture);
+	glTextureParameteri(texture, GL_TEXTURE_MAX_LEVEL, 0);
+	glTextureParameteri(texture, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTextureParameteri(texture, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTextureStorage2D(texture, 1, GL_RGB, w, h);
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+	glTextureSubImage2D(texture, 0, 0, 0, w, h, GL_RGB, GL_UNSIGNED_BYTE, img);
+
+	glBindTextures(0, 1, &texture);
+
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_POLYGON_OFFSET_LINE);
+	glPolygonOffset(-2.0f, -2.0f);
 
 	while (!glfwWindowShouldClose(window))
 	{
 		int width, height;
 		glfwGetFramebufferSize(window, &width, &height);
+		const float ratio = width / (float)height;
+
 		glViewport(0, 0, width, height);
-		glClear(GL_COLOR_BUFFER_BIT);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		const mat4 m = glm::rotate(glm::translate(mat4(1.0f), vec3(0.0f, 0.0f, -3.5f)), (float)glfwGetTime(), vec3(1.0f, 1.0f, 1.0f));
+		const mat4 p = glm::perspective(45.0f, ratio, 0.1f, 1000.0f);
+
+		PerFrameData perFrameData = { p * m, false };
+
+		glNamedBufferSubData(perFrameDataBuffer, 0, kBufferSize, &perFrameData);
+
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+
+		perFrameData.isWireframe = true;
+		glNamedBufferSubData(perFrameDataBuffer, 0, kBufferSize, &perFrameData);
+
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
+	glDeleteBuffers(1, &perFrameDataBuffer);
 	glDeleteProgram(program);
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
